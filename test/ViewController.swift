60cfeee5dcb247046657bc07450b1ae0080b8125
xushiqi <123456>
2020-03-16 16:31:57 +0800

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by xushiqi on 2020/3/16.
+//  Copyright © 2020 Wenzhou Xin Lian Industrial co.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(1)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
